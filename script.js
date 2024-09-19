ab606a849596c6cc748a5371c8d995c28ac4cdc2
lakshay7777 <lakshaykhatri0101@gmail.com>
2024-09-19 18:58:25 +0530

    changing  input type

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
 $(document).ready(function () {
-  
-  
     var data = JSON.parse(localStorage.getItem('data')) || [];
 
-    // Load existing data from localStorage
-    $.each(data, function (index, heading) {
-        $(".heading-select").append(
-            $("<option>").val(heading.heading).text(heading.heading)
-        );
-        $(".heading-form-select").append(
-            $("<option>").val(heading.heading).text(heading.heading)
-        );
-        $(".main-content").append(`
-        <section class="heading-container">
-          <h2>${heading.heading}</h2>
-        </section>
-      `);
+    function saveData() {
+        localStorage.setItem('data', JSON.stringify(data));
+    }
 
-        $.each(heading.subheading, function (subheadingIndex, subheading) {
-            var $headingElement = $(".main-content")
-                .find("h2")
-                .filter(function () {
-                    return $(this).text().trim() === heading.heading;
-                });
-            $headingElement.after(`
-          <div class="subheading-container">
-            <h3>${subheading}</h3>
-          </div>
-        `);
+    function loadData() {
+        $(".main-content").empty();
+        $(".heading-select").empty();
+        $(".heading-form-select").empty();
+
+        $.each(data, function (index, heading) {
+            $(".heading-select").append(
+                $("<option>").val(heading.heading).text(heading.heading)
+            );
+            $(".heading-form-select").append(
+                $("<option>").val(heading.heading).text(heading.heading)
+            );
+            $(".main-content").append(`
+                <section class="heading-container">
+                    <h2>${heading.heading}</h2>
+                </section>
+            `);
+
+            $.each(heading.subheading, function (subheadingIndex, subheading) {
+                var $headingElement = $(".main-content")
+                    .find("h2")
+                    .filter(function () {
+                        return $(this).text().trim() === heading.heading;
+                    });
+                $headingElement.after(`
+                    <div style="margin-left:40px;" class="subheading-container">
+                        <h3>${subheading}</h3>
+                    </div>
+                `);
+            });
+
+            $.each(heading.inputs, function (inputIndex, input) {
+                var $subheadingElement = $(".main-content")
+                    .find("h3")
+                    .filter(function () {
+                        return $(this).text().trim() === input.subheading;
+                    });
+                $subheadingElement.after(`
+                    <div class="input-container">
+                        <label>${input.label}</label>
+                        ${getInputHTML(input)}
+                    </div>
+                `);
+            });
         });
 
-        $.each(heading.inputs, function (inputIndex, input) {
-            var $subheadingElement = $(".main-content")
-                .find("h3")
-                .filter(function () {
-                    return $(this).text().trim() === input.label;
-                });
-            $subheadingElement.after(`
-          <div class="input-container">
-            <label>${input.label}</label>
-            ${getInputHTML(input)}
-          </div>
-        `);
-        });
-    });
+        populateSubheadingsDropdown($(".heading-form-select").val());
+    }
 
     function addHeading(newHeading) {
         $(".heading-select").append(
@@ -53,16 +62,17 @@ $(document).ready(function () {
             $("<option>").val(newHeading).text(newHeading)
         );
         $(".main-content").append(`
-        <section class="heading-container">
-          <h2>${newHeading}</h2>
-        </section>
-      `);
+            <section class="heading-container">
+                <h2>${newHeading}
+                <button type="button" class="close2" aria-label="Close"><span aria-hidden="true">x</span></button></h2>
+            </section>
+        `);
         data.push({
             heading: newHeading,
             subheading: [],
             inputs: []
         });
-        localStorage.setItem('data', JSON.stringify(data));
+        saveData();
         console.log(newHeading, "Heading added");
     }
 
@@ -78,16 +88,16 @@ $(document).ready(function () {
 
         if ($lastSubheadingContainer.length) {
             $lastSubheadingContainer.after(`
-          <div class="subheading-container">
-            <h3>${newSubheading}</h3>
-          </div>
-        `);
+                <div style="margin-left:46px;" class="subheading-container">
+                    <h3>${newSubheading}</h3>
+                </div>
+            `);
         } else {
             $headingElement.after(`
-          <div class="subheading-container">
-            <h3>${newSubheading}</h3>
-          </div>
-        `);
+                <div style="margin-left:10px;" class="subheading-container">
+                    <h3>${newSubheading}</h3>
+                </div>
+            `);
         }
 
         var headingIndex = data.findIndex(function (heading) {
@@ -96,18 +106,21 @@ $(document).ready(function () {
         if (headingIndex !== -1) {
             data[headingIndex].subheading.push(newSubheading);
         }
-        localStorage.setItem('data', JSON.stringify(data));
+        saveData();
         console.log(newSubheading, "Subheading added");
+        
+      
+        populateSubheadingsDropdown(selectedHeading);
     }
 
     function populateSubheadingsDropdown(selectedHeading) {
         var subheadings = [];
-        $(".main-content")
-            .find("h2:contains('" + selectedHeading + "')")
-            .nextAll("div.subheading-container")
-            .each(function () {
-                subheadings.push($(this).find("h3").text());
-            });
+        var headingIndex = data.findIndex(function (heading) {
+            return heading.heading === selectedHeading;
+        });
+        if (headingIndex !== -1) {
+            subheadings = data[headingIndex].subheading;
+        }
 
         var subheadingSelect = $(".subheading-form-select");
         subheadingSelect.empty();
@@ -118,83 +131,47 @@ $(document).ready(function () {
 
     function addFormInput(selectedHeading, selectedSubheading, inputType, options) {
         $(".form").modal("hide");
-        var inputHTML = "";
-
-        if (options.readonly) {
-            inputHTML += ` readonly`;
-        }
-
-        if (options.disabled) {
-            inputHTML += ` disabled`;
-        }
-
-        switch (inputType) {
-            case "input":
-                inputHTML = `<input type="${options.type}" class="${options.class}" placeholder="${options.placeholder}" value="${options.value}"${options.readonly ? " readonly" : ""}${options.disabled ? " disabled" : ""}>`;
-                break;
-            case "textarea":
-                inputHTML = `<textarea class="${options.class}" placeholder="${options.placeholder}"${options.readonly ? " readonly" : ""}${options.disabled ? " disabled" : ""}>${options.value}</textarea>`;
-                break;
-            case "select":
-                inputHTML = `<select class="${options.class}"${options.readonly ? " readonly" : ""}${options.disabled ? " disabled" : ""}>`;
-                $.each(options.option.split(","), function (index, option) {
-                    inputHTML += `<option value="${option}">${option}</option>`;
-                });
-                inputHTML += `</select>`;
-                break;
-            case "checkbox":
-                inputHTML = `<div class="form-check form-check-inline">`;
-                $.each(options.option.split(","), function (index, option) {
-                    inputHTML += `<input type="checkbox" class="${options.class}" value="${option}"${options.readonly ? " readonly" : ""}${options.disabled ? " disabled" : ""}>`;
-                    inputHTML += `<label class="form-check-label">${option}</label>`;
-                });
-                inputHTML += `</div>`;
-                break;
-            case "radio_button":
-                inputHTML = `<div class="form-check form-check-inline">`;
-                $.each(options.option.split(","), function (index, option) {
-                    inputHTML += `<input type="radio" name="radio-group" class="${options.class}" value="${option}"${options.readonly ? " readonly" : ""}${options.disabled ? " disabled" : ""}>`;
-                    inputHTML += `<label class="form-check-label">${option}</label>`;
-                });
-                inputHTML += `</div>`;
-                break;
-            case "button":
-                inputHTML = `<button type="button" class="${options.class}"${options.readonly ? " readonly" : ""}${options.disabled ? " disabled" : ""}>${options.value}</button>`;
-                break;
-        }
+        var inputHTML = getInputHTML({
+            type: inputType,
+            class: options.class,
+            placeholder: options.placeholder,
+            value: options.value,
+            option: options.option,
+            readonly: options.readonly,
+            disabled: options.disabled,
+            required: options.required
+        });
 
         var headingIndex = data.findIndex(function (heading) {
             return heading.heading === selectedHeading;
         });
         if (headingIndex !== -1) {
-            var subheadingIndex = data[headingIndex].subheading.findIndex(function (subheading) {
-                return subheading === selectedSubheading;
+            data[headingIndex].inputs.push({
+                subheading: selectedSubheading,
+                type: inputType,
+                label: options.label,
+                class: options.class,
+                placeholder: options.placeholder,
+                value: options.value,
+                option: options.option,
+                readonly: options.readonly,
+                disabled: options.disabled,
+                required: options.required
             });
-            if (subheadingIndex !== -1) {
-                data[headingIndex].inputs.push({
-                    type: inputType,
-                    label: options.label,
-                    class: options.class,
-                    placeholder: options.placeholder,
-                    value: options.value,
-                    option: options.option,
-                    readonly: options.readonly,
-                    disabled: options.disabled
-                });
-            }
         }
 
         $(".main-content")
             .find("h2:contains('" + selectedHeading + "')")
             .nextAll("div.subheading-container")
             .find("h3:contains('" + selectedSubheading + "')").after(`
-          <div class="input-container">
-            <label>${options.label}</label>
-            ${inputHTML}
-          </div>
-        `);
+                <div style="margin-bottom:20px;" class="input-container">
+                    <label>${options.label}</label>
+                    ${inputHTML}
+                    <br>
+                </div>
+            `);
 
-        localStorage.setItem('data', JSON.stringify(data));
+        saveData();
         console.log("Form input added");
 
         $(".form-inputs").find("input").val("");
@@ -207,23 +184,20 @@ $(document).ready(function () {
     function getInputHTML(input) {
         var inputHTML = "";
         switch (input.type) {
-            case "input":
-                inputHTML = `<input type="${input.type}" class="${input.class}" placeholder="${input.placeholder}" value="${input.value}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}>`;
-                break;
             case "textarea":
-                inputHTML = `<textarea class="${input.class}" placeholder="${input.placeholder}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}>${input.value}</textarea>`;
+                inputHTML = `<textarea class="${input.class}" placeholder="${input.placeholder}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}${input.required ? " required" : ""}>${input.value}</textarea>`;
                 break;
             case "select":
-                inputHTML = `<select class="${input.class}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}>`;
+                inputHTML = `<select class="${input.class}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}${input.required ? " required" : ""}>`;
                 $.each(input.option.split(","), function (index, option) {
-                    inputHTML += `<option value="${option}">${option}</option>`;
+                    inputHTML += `<option value="${option}"${input.value === option ? " selected" : ""}>${option}</option>`;
                 });
                 inputHTML += `</select>`;
                 break;
             case "checkbox":
                 inputHTML = `<div class="form-check form-check-inline">`;
                 $.each(input.option.split(","), function (index, option) {
-                    inputHTML += `<input type="checkbox" class="${input.class}" value="${option}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}>`;
+                    inputHTML += `<input type="checkbox" class="${input.class}" value="${option}"${input.value.includes(option) ? " checked" : ""}${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}${input.required ? " required" : ""}>`;
                     inputHTML += `<label class="form-check-label">${option}</label>`;
                 });
                 inputHTML += `</div>`;
@@ -231,13 +205,13 @@ $(document).ready(function () {
             case "radio_button":
                 inputHTML = `<div class="form-check form-check-inline">`;
                 $.each(input.option.split(","), function (index, option) {
-                    inputHTML += `<input type="radio" name="radio-group" class="${input.class}" value="${option}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}>`;
+                    inputHTML += `<input type="radio" name="radio-group" class="${input.class}" value="${option}"${input.value === option ? " checked" : ""}${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}${input.required ? " required" : ""}>`;
                     inputHTML += `<label class="form-check-label">${option}</label>`;
                 });
                 inputHTML += `</div>`;
                 break;
-            case "button":
-                inputHTML = `<button type="button" class="${input.class}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}>${input.value}</button>`;
+            default:
+                inputHTML = `<input type="${input.type}" class="${input.class}" placeholder="${input.placeholder}" value="${input.value}"${input.readonly ? " readonly" : ""}${input.disabled ? " disabled" : ""}${input.required ? " required" : ""}>`;
                 break;
         }
         return inputHTML;
@@ -268,7 +242,6 @@ $(document).ready(function () {
     $(".heading-form-select").on("change", function () {
         var selectedHeading = $(this).val();
         populateSubheadingsDropdown(selectedHeading);
-        $(".newHeading").val("");
     });
 
     $(".form-inputs").on("submit", function (e) {
@@ -277,14 +250,15 @@ $(document).ready(function () {
         var selectedSubheading = $(".subheading-form-select").val();
         var inputType = $(".choose-input-type").val();
         var inputTypeSpecificOptions = {
-            type: $(".input-type").val(),
+            type: inputType,
             label: $(".input-label").val(),
             class: $(".input-class").val(),
             placeholder: $(".input-placeholder").val(),
             value: $(".input-value").val(),
             option: $(".input-option").val(),
             readonly: $(".input-readonly").prop("checked"),
-            disabled: $(".input-disabled").prop("checked")
+            disabled: $(".input-disabled").prop("checked"),
+            required: $(".input-required").prop("checked")
         };
 
         if (inputTypeSpecificOptions.readonly && inputTypeSpecificOptions.disabled) {
@@ -295,5 +269,46 @@ $(document).ready(function () {
         if (selectedHeading && selectedSubheading && inputType) {
             addFormInput(selectedHeading, selectedSubheading, inputType, inputTypeSpecificOptions);
         }
+    });
+
+    
+    $(document).on('change', '.input-container input, .input-container textarea, .input-container select', function() {
+        var $input = $(this);
+        var value = $input.val();
+        if ($input.attr('type') === 'checkbox') {
+            value = [];
+            $input.closest('.form-check-inline').find('input:checked').each(function() {
+                value.push($(this).val());
+            });
+        }
+        if ($input.attr('type') === 'radio') {
+            value = $input.closest('.form-check-inline').find('input:checked').val();
+        }
+
+        var headingText = $input.closest('.heading-container').find('h2').text();
+        var subheadingText = $input.closest('.subheading-container').find('h3').text();
+        
+        var headingIndex = data.findIndex(function(heading) {
+            return heading.heading === headingText;
+        });
+
+        if (headingIndex !== -1) {
+            var inputIndex = data[headingIndex].inputs.findIndex(function(input) {
+                return input.subheading === subheadingText && input.label === $input.prev('label').text();
+            });
+
+            if (inputIndex !== -1) {
+                data[headingIndex].inputs[inputIndex].value = value;
+                saveData();
+            }
+        }
+    });
+
+
+    loadData();
+
+    $('.form').on('show.bs.modal', function () {
+        var selectedHeading = $(".heading-form-select").val();
+        populateSubheadingsDropdown(selectedHeading);
     });
 });
